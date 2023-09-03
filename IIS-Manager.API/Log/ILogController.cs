bd58f5857a727cada8eb527d7f4cdef425395e55
Knabbermann <jiri.skoda@gmx.de>
2023-09-03 14:19:44 +0200

    removed logs from .gitignore

---
+++ IIS-Manager.API/Log/ILogController.cs
@@ -0,0 +1,10 @@
+﻿using IIS_Manager.Utility;
+
+namespace IIS_Manager.Controllers.Log
+{
+    public interface ILogController
+    {
+        public List<Models.Log> GetAllLogs();
+        public void Log(string message, string userId, string? type = StaticDetails.LogTypeInformation);
+    }
+}
